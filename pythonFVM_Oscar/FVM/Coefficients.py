606261e5da5cfeb76799f9f658bae90cdd2bd3e1
OscarAlvarado <OscarAlvarado@ciencias.unam.mx>
2021-02-20 20:33:31 -0600

    Se quitaron de ser anonimos unos atributos para poder acceder más fácil

--- pythonFVM_Oscar/FVM/Coefficients.py
+++ pythonFVM_Oscar/FVM/Coefficients.py
@@ -20,7 +20,7 @@ class Coefficients():
     """    
 
     def __init__(self, mesh):
-        self._dim = mesh.dim
+        self.dim = mesh.dim
         self._mesh = mesh
         self._volumes = mesh.volumes
         self._Su = np.zeros(self._volumes)  ;  self._Sp = None
@@ -32,19 +32,19 @@ class Coefficients():
         Método que inicializa los coeficientes en un arreglo tridimensional
         """
         vols = self._volumes
-        self._aP = np.zeros(vols)
-        dim = self._dim
-        self._aE = np.zeros(vols)
-        self._aW = np.zeros(vols)
-        self._Sp = np.zeros(vols)
-        self._Su = np.zeros(vols)
-        if dim > 1:
-            self._aN = np.zeros(vols)
-            self._aS = np.zeros(vols)
-        if dim == 3:
-            self._aT = np.zeros(vols)
-            self._aB = np.zeros(vols)
-        
+        self.aP = np.zeros(vols)
+        dim = self.dim
+        self.aE = np.zeros(vols)
+        self.aW = np.zeros(vols)
+        self.Sp = np.zeros(vols)
+        self.Su = np.zeros(vols)
+
+        self.aN = np.zeros(vols)
+        self.aS = np.zeros(vols)
+
+        self.aT = np.zeros(vols)
+        self.aB = np.zeros(vols)
+
     def set_diffusion(self, gamma):
         """This method makes an instance of the Diffusion class and uses it to
         update the aP,aE,aW, ... atributes.
@@ -54,57 +54,57 @@ class Coefficients():
                a function of the coordinates, denoted as Gamma=f(x,y,z) should be 
                used. (float or function)"""
         
-        dim = self._dim
+        dim = self.dim
         malla = self._mesh
         diffusion = Diffusion(malla, gamma)
         east_diff, sp_e, su_e = diffusion.east_diffusion()
         west_diff, sp_w, su_w = diffusion.west_diffusion()
-        self._aE -= east_diff
-        self._aW -= west_diff
-        self._Sp -= sp_e + sp_w
-        self._Su += su_e + su_w
-        self._aP +=  self._aE + self._aW
+        self.aE -= east_diff
+        self.aW -= west_diff
+        self.Sp -= sp_e + sp_w
+        self.Su += su_e + su_w
+        self.aP -=  self.aE + self.aW
         if dim > 1:
             north_diff, sp_n, su_n = diffusion.north_diffusion()
             south_diff, sp_s, su_s = diffusion.south_diffusion()
-            self._aN -= north_diff
-            self._aS -= south_diff
-            self._Sp -= sp_n + sp_s
-            self._Su += su_n + su_s
-            self._aP += self._aN + self._aS
+            self.aN -= north_diff
+            self.aS -= south_diff
+            self.Sp -= sp_n + sp_s
+            self.Su += su_n + su_s
+            self.aP -= self.aN + self.aS
         if dim == 3:
             top_diff, sp_t, su_t = diffusion.top_diffussion()
             bottom_diff, sp_b, su_b = diffusion.bottom_diffusion()
-            self._aT -= top_diff
-            self._aB -= bottom_diff
-            self._Sp -= sp_t + sp_b
-            self._Su += su_t + su_b
-            self._aP += top_diff + bottom_diff
-        self._aP += self._Sp
+            self.aT -= top_diff
+            self.aB -= bottom_diff
+            self.Sp -= sp_t + sp_b
+            self.Su += su_t + su_b
+            self.aP -= top_diff + bottom_diff
+        self.aP -= self.Sp
             
     def get_Su(self):
-        return self._Su
+        return self.Su
     
     def get_Sp(self):
-        return self._Sp
+        return self.Sp
     
     def get_aP(self):
-        return self._aP
+        return self.aP
     
     def get_aE(self):
-        return self._aE
+        return self.aE
     
     def get_aW(self):
-        return self._aW
+        return self.aW
 
     def get_aN(self):
-        return self._aN
+        return self.aN
 
     def get_aS(self):
-        return self._aS
+        return self.aS
 
     def get_aT(self):
-        return self._aT
+        return self.aT
 
     def get_aB(self):
-        return self._aB
+        return self.aB
