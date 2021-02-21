2cb47c8b3fae1b21f1e6053bfd180bee391d2365
OscarAlvarado <OscarAlvarado@ciencias.unam.mx>
2021-02-21 04:06:20 -0600

    Se actualizó la parte de la convección, queda muy parecida a la de difusión

--- pythonFVM_Oscar/FVM/Coefficients.py
+++ pythonFVM_Oscar/FVM/Coefficients.py
@@ -1,16 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  3 21:14:10 2019
-
-@author: jose
-"""
-
 import numpy as np
-#from numpy import maximum as maxi
 from Diffusion import Diffusion
-#import scipy.sparse as sp
-
+from Convection import Convection
 class Coefficients():
     """
     Class that defines the coefficients that every node needs to form it's
@@ -81,6 +71,57 @@ class Coefficients():
             self.Su += su_t + su_b
             self.aP -= top_diff + bottom_diff
         self.aP -= self.Sp
+        
+    def set_convection(self, rho, vel, scheme='central'):
+        """
+        This method makes an instance of the convection class and uses it to
+        update the aP, aE, aW, ... atributes.
+        
+        rho: float or 3D array that represents the density, if a float is used
+             it is assumed that the density is constant in all the physical 
+             domain. (float/numpy array)
+        
+        vel: tuple of 3D arrays that represents the velocity, each 3D array in
+             the tuple represent a component of the velocity. It can be denoted 
+             as vel=(u,v,w) where u, v and w are 3D arrays with values for all
+             of the volume borders positions. (tuple of numpy arrays) 
+        
+        scheme: scheme to be used for the convection terms. Strings 'central' and 
+                'upwind1' are currently valid (string).
+        """
+        
+        dim = self.dim
+        malla = self._mesh
+        convection = Convection(malla, rho, vel, scheme)
+        east_conv, sp_e, su_e = convection.east_convection()
+        west_conv, sp_w, su_w = convection.west_convection()
+        east_f = convection.get_f(east_conv, "E")
+        west_f = convection.get_f(west_conv, "W")
+        self.aE += east_f
+        self.aW -= west_f
+        self.aP -= east_f + sp_e - sp_w - west_f
+        self.Su += su_e + su_w
+        self.Sp -= -sp_e + sp_w
+        if dim > 1:
+            north_conv, sp_n, su_n = convection.south_convection()
+            south_conv, sp_s, su_s = convection.south_convection()
+            north_f = convection.get_f(north_conv, "N")
+            south_f = convection.get_f(south_conv, "S")
+            self.aE += north_f
+            self.aW -= south_f
+            self.aP -= north_f + sp_n - sp_s - south_f
+            self.Su += su_n + su_s
+            self.Sp -= -sp_n + sp_s
+        if dim == 3:
+            top_conv, sp_t, su_t = convection.top_convection()
+            bottom_conv, sp_b, su_b = convection.bottom_convection()
+            top_f = convection.get_f(top_conv, "T")
+            bottom_f = convection.get_f(bottom_conv, "B")
+            self.aE += top_f
+            self.aW -= bottom_f
+            self.aP -= top_f + sp_t - sp_b - bottom_f
+            self.Su += su_t + su_b
+            self.Sp -= -sp_t + sp_b
             
     def get_Su(self):
         return self.Su
