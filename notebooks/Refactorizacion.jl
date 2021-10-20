a41a544918f2a68f5b37500906cbd6ff30ef1dad
OscarAlvarado <OscarAlvarado@ciencias.unam.mx>
2021-10-20 03:24:51 -0500

    Creación de notebook

---
+++ notebooks/Refactorizacion.jl
@@ -0,0 +1,68 @@
+### A Pluto.jl notebook ###
+# v0.14.7
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a5c6a6a8-317a-11ec-25bc-7d7e08c53eb4
+using PlutoUI
+
+# ╔═╡ 014b9495-588e-4485-a26f-81807624d7b5
+md"""
+# Método del Volumen Finito
+- Óscar A. Alvarado-Morán
+- Oscar A. Esquivel-flores
+"""
+
+
+# ╔═╡ 4dbf25be-9427-404c-ac08-7e44f126ec6e
+PlutoUI.TableOfContents(aside=true, title="🎣 Método del Volumen Finito 🔥")
+
+# ╔═╡ 226a6ac4-3f23-454a-9d7c-737cc316e11c
+md"## Mallado"
+
+# ╔═╡ de5fb957-0121-4830-974a-4587259da8fb
+struct 
+
+# ╔═╡ b5ed1976-b09a-4a60-83ce-2455d5d63755
+md"## Coeficientes"
+
+# ╔═╡ 49079a92-d6c5-47cd-8d23-b7f6a7012c19
+
+
+# ╔═╡ 1415d40a-91bb-4c26-9d31-23ad698285dd
+md"## Difusión"
+
+# ╔═╡ a8b43883-37de-4f20-aa2d-97816d13344b
+
+
+# ╔═╡ aaa69ed2-d5ad-4bc8-825f-1e65b4d36da2
+md"## Advección"
+
+# ╔═╡ 07bb4c2d-3e19-4b3d-996b-5d7eba5fbd70
+
+
+# ╔═╡ 3f1cd4d3-9052-4271-9e4e-8965c4e99170
+md"## Sistema de ecuaciones"
+
+# ╔═╡ 35fb94be-3647-4156-b82e-b54baa167fba
+
+
+# ╔═╡ a934ecd4-5d4c-4764-a518-0fb01aeee45d
+md"## Ecuación de Poisson 3D"
+
+# ╔═╡ Cell order:
+# ╟─014b9495-588e-4485-a26f-81807624d7b5
+# ╟─a5c6a6a8-317a-11ec-25bc-7d7e08c53eb4
+# ╟─4dbf25be-9427-404c-ac08-7e44f126ec6e
+# ╟─226a6ac4-3f23-454a-9d7c-737cc316e11c
+# ╠═de5fb957-0121-4830-974a-4587259da8fb
+# ╟─b5ed1976-b09a-4a60-83ce-2455d5d63755
+# ╠═49079a92-d6c5-47cd-8d23-b7f6a7012c19
+# ╟─1415d40a-91bb-4c26-9d31-23ad698285dd
+# ╠═a8b43883-37de-4f20-aa2d-97816d13344b
+# ╟─aaa69ed2-d5ad-4bc8-825f-1e65b4d36da2
+# ╠═07bb4c2d-3e19-4b3d-996b-5d7eba5fbd70
+# ╟─3f1cd4d3-9052-4271-9e4e-8965c4e99170
+# ╠═35fb94be-3647-4156-b82e-b54baa167fba
+# ╟─a934ecd4-5d4c-4764-a518-0fb01aeee45d
